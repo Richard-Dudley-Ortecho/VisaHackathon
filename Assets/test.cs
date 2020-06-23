1bda13842c3a8f0b4c9dd07ecbadb959d4c682ab
Eric Ying <eric99ying@gmail.com>
2020-06-23 12:26:01 -0700

    Got GET request working

---
+++ Assets/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vdp;
+using System;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    	Debug.Log("Debugging start");
+        VisaAPIClient visaAPIClient = new VisaAPIClient();
+
+        string baseUri = "vmorc/offers";
+        string resourcePath = "/v1/all";
+        string queryParameters = "?max_offers=5";
+        string status = visaAPIClient.DoMutualAuthCall(baseUri + resourcePath + queryParameters, "GET", "Merchant Offer API Test", "");
+
+        Debug.Log(status);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
