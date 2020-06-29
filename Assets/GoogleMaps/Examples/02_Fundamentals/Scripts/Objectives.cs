a09f24cb0deb19fecdb88aba228cdd936f159b36
Jacob Wahbeh <wahbehj18@g.ucla.edu>
2020-06-29 14:09:06 -0700

    Added Objectives Panel

---
+++ Assets/GoogleMaps/Examples/02_Fundamentals/Scripts/Objectives.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives : MonoBehaviour
+{
+    public GameObject panel;
+    // Start is called before the first frame update
+    public void OpenTab()
+    {
+        if (panel != null)
+        {
+            bool isActive = panel.activeSelf;
+            panel.SetActive(!isActive);
+        }
+    }
+}
